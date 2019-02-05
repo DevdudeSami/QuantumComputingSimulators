5763436bf6ba250029edd9712eea2583d76531ae
Youssef Moawad <youssef_programming@icloud.com>
2019-02-05 16:10:10 +0000

    Initialisation of no-matrix Xcode project

---
+++ NoMatrixSimulator/NoMatrixSimulator/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  NoMatrixSimulator
+//
+//  Created by Youssef Moawad on 05/02/2019.
+//  Copyright © 2019 Youssef Moawad. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
