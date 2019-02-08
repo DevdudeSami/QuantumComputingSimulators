e98d0cddc6774e27bd89bbf7f46951157d291736
Youssef Moawad <youssef_programming@icloud.com>
2019-02-08 12:14:38 +0000

    Changed the NM python simulator to be to do Cn operations. Other minor changes.

--- SerialPythonSimulator/NMQRegister.py
+++ SerialPythonSimulator/NMQRegister.py
@@ -21,7 +21,6 @@ class NMQRegister:
 		for i in range(0, 2**(self.n-1)):
 			zero_state = nth_cleared(i, qID)
 			one_state = zero_state | (1 << qID)
-
 			zero_amp = self.stateVector[zero_state]
 			one_amp = self.stateVector[one_state]
 
@@ -44,24 +43,27 @@ class NMQRegister:
 			if(control_val_zero == 1): self.stateVector[zero_state] = gate[0]*zero_amp + gate[1]*one_amp
 			if(control_val_one): self.stateVector[one_state] = gate[2]*zero_amp + gate[3]*one_amp
 
-	def applyCCGate(self, C1, C2, targetQID, gate):
+	def applyCnGate(self, controls, targetQID, gate):
 		assert(len(gate) == 4)
 
 		for i in range(0, 2**(self.n-1)):
 			zero_state = nth_cleared(i, targetQID)
 			one_state = zero_state | (1 << targetQID)
 
-			control1_val_zero = 1 if (((1 << C1) & zero_state) > 0) else 0
-			control1_val_one = 1 if (((1 << C1) & one_state) > 0) else 0
+			control_zero = True
+			control_one = True
 
-			control2_val_zero = 1 if (((1 << C2) & zero_state) > 0) else 0
-			control2_val_one = 1 if (((1 << C2) & one_state) > 0) else 0
+			for c in range(0, len(controls)):
+				if(not (((1 << controls[c]) & zero_state) > 0)): control_zero = False
+				if(not (((1 << controls[c]) & one_state) > 0)): control_one = False
+				
+				if(not control_zero and not control_one): break
 
 			zero_amp = self.stateVector[zero_state]
 			one_amp = self.stateVector[one_state]
 
-			if(control1_val_zero == 1 and control2_val_zero == 1): self.stateVector[zero_state] = gate[0]*zero_amp + gate[1]*one_amp
-			if(control1_val_one == 1 and control2_val_one == 1): self.stateVector[one_state] = gate[2]*zero_amp + gate[3]*one_amp
+			if(control_zero): self.stateVector[zero_state] = gate[0]*zero_amp + gate[1]*one_amp
+			if(control_one): self.stateVector[one_state] = gate[2]*zero_amp + gate[3]*one_amp
 
 	def measure(self):
 		probs = []
@@ -80,10 +82,10 @@ class NMQRegister:
 H = [1/sqrt(2)+0j, 1/sqrt(2)+0j, 1/sqrt(2)+0j, -1/sqrt(2)+0j]
 X = [0j, 1+0j, 1+0j, 0j]
 
-myReg = NMQRegister(3)
-myReg.applySingleGate(0, H)
-myReg.applySingleGate(1, H)
-myReg.applyCCGate(0,1,2,X)
+myReg = NMQRegister(10)
+# myReg.applySingleGate(8, X)
+# myReg.applySingleGate(7, X)
+myReg.applyCnGate([7,8],2,X)
 # myReg.applySingleGate(1, H)
 # myReg.applySingleGate(2, H)
 
